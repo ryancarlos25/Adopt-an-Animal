f963450f75d8f3c4f5839c5d7726d7ad90b5c260
ryancarlos25 <ryan-2002-@hotmail.com>
2022-04-13 10:06:53 -0300

    Register Form

---
+++ main.js
@@ -0,0 +1,9 @@
+'use-strict';
+
+const pesquisarCep = () =+ {
+        const cep = document.getElementById('cep').value;
+        console.log(cep);
+}
+
+document.getElementById('cep')
+        .addEventListener('focusout', pesquisarCep;)
